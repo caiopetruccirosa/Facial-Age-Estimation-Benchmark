326d1fcf6aa6b067bea23e7e6610dbf8e7c639d5
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2024-09-03 01:01:07 -0300

    update run_morph_only script

--- scripts/benchmarks/run_morph_only_benchmark.sh
+++ scripts/benchmarks/run_morph_only_benchmark.sh
@@ -1,11 +1,11 @@
 mkdir -p logs/benchmarks/MORPH_only
 
-python prepare_data.py facebase/benchmarks/MORPH_only.yaml &> logs/benchmarks/MORPH_only/prepare_data.log
+python prepare_data.py facebase/benchmarks/MORPH_only.yaml 2>&1 | tee -a logs/benchmarks/MORPH_only/prepare_data.log
 
-python train.py facebase/benchmarks/MORPH_only.yaml 0 &> logs/benchmarks/MORPH_only/train_split0.log
-python train.py facebase/benchmarks/MORPH_only.yaml 1 &> logs/benchmarks/MORPH_only/train_split1.log
-python train.py facebase/benchmarks/MORPH_only.yaml 2 &> logs/benchmarks/MORPH_only/train_split2.log
-python train.py facebase/benchmarks/MORPH_only.yaml 3 &> logs/benchmarks/MORPH_only/train_split3.log
-python train.py facebase/benchmarks/MORPH_only.yaml 4 &> logs/benchmarks/MORPH_only/train_split4.log
+python train.py facebase/benchmarks/MORPH_only.yaml 0 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split0.log
+python train.py facebase/benchmarks/MORPH_only.yaml 1 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split1.log
+python train.py facebase/benchmarks/MORPH_only.yaml 2 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split2.log
+python train.py facebase/benchmarks/MORPH_only.yaml 3 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split3.log
+python train.py facebase/benchmarks/MORPH_only.yaml 4 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split4.log
 
-python evaluate.py facebase/benchmarks/MORPH_only.yaml &> logs/benchmarks/MORPH_only/evaluate.log
+python evaluate.py facebase/benchmarks/MORPH_only.yaml 2>&1 | tee -a logs/benchmarks/MORPH_only/evaluate.log
