cd8a21d63cfd1972ac6249761413b91d48f75a19
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2024-09-03 01:09:26 -0300

    fix run_morph_only benchmark script

--- scripts/benchmarks/run_morph_only_benchmark.sh
+++ scripts/benchmarks/run_morph_only_benchmark.sh
@@ -1,11 +1,11 @@
-mkdir -p logs/benchmarks/MORPH_only
+mkdir -p logs/experiments/MORPH_only_256x256
 
-python prepare_data.py facebase/benchmarks/MORPH_only.yaml 2>&1 | tee -a logs/benchmarks/MORPH_only/prepare_data.log
+python prepare_data.py facebase/configs/MORPH_only_256x256.yaml 2>&1 | tee -a logs/experiments/MORPH_only_256x256/prepare_data.log
 
-python train.py facebase/benchmarks/MORPH_only.yaml 0 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split0.log
-python train.py facebase/benchmarks/MORPH_only.yaml 1 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split1.log
-python train.py facebase/benchmarks/MORPH_only.yaml 2 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split2.log
-python train.py facebase/benchmarks/MORPH_only.yaml 3 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split3.log
-python train.py facebase/benchmarks/MORPH_only.yaml 4 2>&1 | tee -a logs/benchmarks/MORPH_only/train_split4.log
+python train.py facebase/configs/MORPH_only_256x256.yaml 0 2>&1 | tee -a logs/experiments/MORPH_only_256x256/train_split0.log
+python train.py facebase/configs/MORPH_only_256x256.yaml 1 2>&1 | tee -a logs/experiments/MORPH_only_256x256/train_split1.log
+python train.py facebase/configs/MORPH_only_256x256.yaml 2 2>&1 | tee -a logs/experiments/MORPH_only_256x256/train_split2.log
+python train.py facebase/configs/MORPH_only_256x256.yaml 3 2>&1 | tee -a logs/experiments/MORPH_only_256x256/train_split3.log
+python train.py facebase/configs/MORPH_only_256x256.yaml 4 2>&1 | tee -a logs/experiments/MORPH_only_256x256/train_split4.log
 
-python evaluate.py facebase/benchmarks/MORPH_only.yaml 2>&1 | tee -a logs/benchmarks/MORPH_only/evaluate.log
+python evaluate.py facebase/configs/MORPH_only_256x256.yaml 2>&1 | tee -a logs/experiments/MORPH_only_256x256/evaluate.log
